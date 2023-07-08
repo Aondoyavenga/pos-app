c1bcbf4d0301075d437d8a1f693859ae2b1e154e
= <=>
2023-07-09 00:54:13 +0100

    store

--- server/src/routes/storeRoute.js
+++ server/src/routes/storeRoute.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import { createStore, getStores } from '../methods/StoreMethod.js'
+import { getStores, createStore }  from '../methods/storeMethod.js'
+
 const router = express.Router()
 
 router
     .get('/', getStores)
     .post('/', createStore)
-
 
 export const STORE_ROUTER = {
     router
